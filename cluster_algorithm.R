211d0ff58e94e2a57792193e1a1110e83ecd03a2
Stuart Sumner <stuartsumner@Stuarts-MBP.cable.rcn.com>
2023-09-11 20:56:35 -0500

    Cluster algorithm

--- cluster_algorithm.R
+++ cluster_algorithm.R
@@ -36,9 +36,7 @@ library(NbClust)
 library(factoextra)
 
 fviz_nbclust(customer_data[,3:5], kmeans, method = "silhouette")
-
-set.seed(125)
-stat_gap <- clusGap(customer_data[,3:5], FUN = kmeans, nstart = 25,
+stat_gap <- cluster::clusGap(customer_data[,3:5], FUN = kmeans, nstart = 25,
                     K.max = 10, B = 50)
 fviz_gap_stat(stat_gap)
 
@@ -52,3 +50,34 @@ pcclust=prcomp(customer_data[,3:5],scale=FALSE) #principal component analysis
 summary(pcclust)
 
 pcclust$rotation[,1:2]
+ggplot(customer_data, aes(x =`Annual Income (k$)`, y = `Spending Score (1-100)`)) + 
+  geom_point(stat = "identity", aes(color = as.factor(k6$cluster))) +
+  scale_color_discrete(name=" ",
+                       breaks=c("1", "2", "3", "4", "5","6"),
+                       labels=paste0("Cluster ", seq(1:6))) +
+  ggtitle("Segments of Mall Customers", subtitle = "Using K-means Clustering") +
+  ggthemes::theme_fivethirtyeight()
+
+
+
+ggplot(customer_data, aes(x =`Spending Score (1-100)`, y =Age)) + 
+  geom_point(stat = "identity", aes(color = as.factor(k6$cluster)), size = 2.5, alpha = .7) +
+  scale_color_discrete(name=" ",
+                       breaks=c("1", "2", "3", "4", "5","6"),
+                       labels=c("Cluster 1", "Cluster 2", "Cluster 3", "Cluster 4", "Cluster 5","Cluster 6")) +
+  ggtitle("Segments of Mall Customers", subtitle = "Using K-means Clustering") +
+  theme_minimal()
+
+
+
+kCols=function(vec){cols=rainbow (length (unique (vec)))
+return (cols[as.numeric(as.factor(vec))])}
+
+digCluster<-k6$cluster; dignm<-as.character(digCluster); # K-means clusters
+
+plot(pcclust$x[,1:2], col =kCols(digCluster), 
+     pch =19, xlab ="K-means", ylab="classes")
+legend("bottomleft",unique(dignm),fill=unique(kCols(digCluster)))
+?rainbow
+cols=rainbow (length (unique (vec)))
+              
